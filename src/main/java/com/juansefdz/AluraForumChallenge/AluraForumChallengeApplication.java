883289d560cdd26d54e0f12698bcfe9230cfe147
juansefdz <jsfm81091@gmail.com>
2024-11-15 13:34:58 -0500

    innitial commit  -  folder configuration in hexagonal mode - pom settings

---
+++ src/main/java/com/juansefdz/AluraForumChallenge/AluraForumChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.juansefdz.AluraForumChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForumChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForumChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juansefdz/AluraForumChallenge/AluraForumChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansefdz.AluraForumChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForumChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
